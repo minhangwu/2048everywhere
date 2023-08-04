b14beefbaf21a75d64897c85279171f79fe7eceb
minhang <wu.minhang@outlook.com>
2023-08-04 17:48:09 +0800

    Optimize code structure

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const moveTiles = require('./move');
-
-var tiles = []
-var size = 4
+var tiles = [];
+var size = 4;
+var score = 0;
 
 // Create size*size tiles with default value of 0
 function createBoard() {
@@ -35,6 +34,10 @@ function updateBoard() {
     }
 }
 
+function updateScore() {
+    console.log(score);
+}
+
 // Add a tile at a random empty location with value of 2 or 4
 function addTile() {
     let row, col = 0;
@@ -60,6 +63,7 @@ function randomSelector(option1, option2) {
     }
 }
 
+// Return if two array of tiles equal
 function tilesEqual(tiles1, tiles2) {
     for (let i=0;i<tiles1.length;i++) {
         for (let j=0;j<tiles1.length;j++) {
@@ -71,6 +75,99 @@ function tilesEqual(tiles1, tiles2) {
     return true;
 }
 
+function slide(row) {
+    let length = row.length;
+
+    // Remove zeros
+    row = row.filter(value => value !==0);
+
+    // Join tiles
+    for (let i=1;i<row.length;i++) {
+        if (row[i-1] === row[i]) {
+            row[i-1] *= 2;
+            row[i] = 0;
+            // Add score
+            score += row[i-1];
+        }
+    }
+    
+    // Remove zeros
+    row = row.filter(value => value !==0);
+
+    // Add back zeros to end of row
+    for (let i=0;i<length;i++) {
+        if (!row[i]) {
+            row[i] = 0;
+        }
+    }
+
+    return row;
+}
+
+function transformTiles(tiles, operation) {
+    let temp = [];
+
+    for (let i = 0; i < tiles.length; i++) {
+        temp[i] = [];
+        for (let j = 0; j < tiles.length; j++) {
+            let newI, newJ;
+            if (operation === "transpose") {
+                newI = j;
+                newJ = i;
+            } else if (operation === "flip") {
+                newI = i;
+                newJ = tiles.length - 1 - j;
+            }
+            temp[i][j] = tiles[newI][newJ];
+        }
+    }
+
+    return temp;
+}
+
+function moveTiles(tiles, direction) {
+    // Make a deep copy of tiles
+    let temp = JSON.parse(JSON.stringify(tiles));
+
+    // Transform tiles for slide function
+    switch (direction) {
+        case 'left':
+            break;
+        case 'up':
+            temp = transformTiles(temp, 'transpose');
+            break;
+        case 'right':
+            temp = transformTiles(temp, 'flip');
+            break;
+        case 'down':
+            temp = transformTiles(temp, 'transpose');
+            temp = transformTiles(temp, 'flip');
+            break;
+    }
+
+    for (let i=0;i<temp.length;i++) {
+        temp[i] = slide(temp[i]);
+    }
+
+    // Transform back to original form
+    switch (direction) {
+        case 'left':
+            break;
+        case 'up':
+            temp = transformTiles(temp, 'transpose');
+            break;
+        case 'right':
+            temp = transformTiles(temp, 'flip');
+            break;
+        case 'down':
+            temp = transformTiles(temp, 'flip');
+            temp = transformTiles(temp, 'transpose');
+            break;
+    }
+
+    return temp;
+}
+
 function move(direction) {
     let newTiles = moveTiles(tiles, direction);
     
@@ -78,13 +175,16 @@ function move(direction) {
         tiles = newTiles;
         addTile();
         updateBoard();
+        updateScore();
 
         if (hasWon()) {
             alert('You Won!');
             createBoard();
+            score = 0;
         } else if (isGameOver()) {
             alert('Game Over!');
             createBoard();
+            score = 0;
         }
     }
 }
@@ -145,7 +245,6 @@ document.addEventListener('keyup', (key) => {
         case 'KeyS':
             move('down');
             break;
+        }
     }
-})
-
-module.exports = moveLeft;
+)
